1d0cbb8f95c6ad4817dd0870f967c7547dcc7760
Edson Shideki Kokado <eskokado@gmail.com>
2020-12-30 17:44:07 -0300

    Developed test whenBeerInformedThenItShouldBeCreated

---
+++ src/test/java/org/esk/diobeerstockapi/services/BeerServiceTest.java
@@ -0,0 +1,48 @@
+package org.esk.diobeerstockapi.services;
+
+import org.esk.diobeerstockapi.builders.BeerDTOBuilder;
+import org.esk.diobeerstockapi.dtos.BeerDTO;
+import org.esk.diobeerstockapi.entities.Beer;
+import org.esk.diobeerstockapi.exceptions.BeerAlreadyRegisteredException;
+import org.esk.diobeerstockapi.mappers.BeerMapper;
+import org.esk.diobeerstockapi.repositories.BeerRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class BeerServiceTest {
+    @Mock
+    private BeerRepository beerRepository;
+
+    private BeerMapper beerMapper = BeerMapper.INSTANCE;
+
+    @InjectMocks
+    private BeerService beerService;
+
+    @Test
+    void whenBeerInformedThenItShouldBeCreated() throws BeerAlreadyRegisteredException {
+        // given
+        BeerDTO expectedBeerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+        Beer expectedSavedBeer = beerMapper.toModel(expectedBeerDTO);
+
+        // when
+        when(beerRepository.findByName(expectedBeerDTO.getName())).thenReturn(Optional.empty());
+        when(beerRepository.save(expectedSavedBeer)).thenReturn(expectedSavedBeer);
+
+        //then
+        BeerDTO createdBeerDTO = beerService.createBeer(expectedBeerDTO);
+
+        assertThat(createdBeerDTO.getId(), is(equalTo(expectedBeerDTO.getId())));
+        assertThat(createdBeerDTO.getName(), is(equalTo(expectedBeerDTO.getName())));
+        assertThat(createdBeerDTO.getQuantity(), is(equalTo(expectedBeerDTO.getQuantity())));
+    }
+}
