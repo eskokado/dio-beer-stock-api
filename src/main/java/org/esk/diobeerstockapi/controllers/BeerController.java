afdf40f375e3af3841c903f9bdce66587cc35cfe
Edson Shideki Kokado <eskokado@gmail.com>
2020-12-30 16:10:48 -0300

    Developed BeerController

---
+++ src/main/java/org/esk/diobeerstockapi/controllers/BeerController.java
@@ -0,0 +1,42 @@
+package org.esk.diobeerstockapi.controllers;
+
+import lombok.AllArgsConstructor;
+import org.esk.diobeerstockapi.dtos.BeerDTO;
+import org.esk.diobeerstockapi.exceptions.BeerAlreadyRegisteredException;
+import org.esk.diobeerstockapi.exceptions.BeerNotFoundException;
+import org.esk.diobeerstockapi.services.BeerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/beers")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class BeerController {
+    private final BeerService beerService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BeerDTO createBeer(@RequestBody @Valid BeerDTO beerDTO) throws BeerAlreadyRegisteredException {
+        return beerService.createBeer(beerDTO);
+    }
+
+    @GetMapping("/{name}")
+    public BeerDTO findByName(@PathVariable String name) throws BeerNotFoundException {
+        return beerService.findByName(name);
+    }
+
+    @GetMapping
+    public List<BeerDTO> listBeers() {
+        return beerService.listAll();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws BeerNotFoundException {
+        beerService.deleteById(id);
+    }
+}
