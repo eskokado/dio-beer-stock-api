58564bd109d188ab4001b002ebe9b8ced5d4c1bc
Edson Shideki Kokado <eskokado@gmail.com>
2020-12-30 17:22:42 -0300

    Developed BeerDTOBuilder

---
+++ src/test/java/org/esk/diobeerstockapi/builders/BeerDTOBuilder.java
@@ -0,0 +1,35 @@
+package org.esk.diobeerstockapi.builders;
+
+import lombok.Builder;
+import org.esk.diobeerstockapi.dtos.BeerDTO;
+import org.esk.diobeerstockapi.enums.BeerType;
+
+@Builder
+public class BeerDTOBuilder {
+    @Builder.Default
+    private Long id = 1L;
+
+    @Builder.Default
+    private String name = "Brahma";
+
+    @Builder.Default
+    private String brand = "Ambev";
+
+    @Builder.Default
+    private int max = 50;
+
+    @Builder.Default
+    private int quantity = 10;
+
+    @Builder.Default
+    private BeerType type = BeerType.LAGER;
+
+    public BeerDTO toBeerDTO() {
+        return new BeerDTO(id,
+                name,
+                brand,
+                max,
+                quantity,
+                type);
+    }
+}
