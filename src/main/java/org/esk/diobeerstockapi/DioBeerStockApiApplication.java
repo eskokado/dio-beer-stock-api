bfb2c8ace1259b3e36f5bcdc5bb83722af6a8c1f
Edson Shideki Kokado <eskokado@gmail.com>
2020-12-30 14:14:27 -0300

    Initial Commit

---
+++ src/main/java/org/esk/diobeerstockapi/DioBeerStockApiApplication.java
@@ -0,0 +1,13 @@
+package org.esk.diobeerstockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioBeerStockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioBeerStockApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/esk/diobeerstockapi/DioBeerStockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.esk.diobeerstockapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioBeerStockApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
