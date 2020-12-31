04bcc5af9e2c78817fbd4efb16610cefde7a0022
Edson Shideki Kokado <eskokado@gmail.com>
2020-12-31 06:09:32 -0300

    Developed test whenAlreadyRegisteredBeerInformedThenAnExceptionShouldBeThrown

--- src/test/java/org/esk/diobeerstockapi/services/BeerServiceTest.java
+++ src/test/java/org/esk/diobeerstockapi/services/BeerServiceTest.java
@@ -16,6 +16,7 @@ import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -46,5 +47,18 @@ public class BeerServiceTest {
         assertThat(createdBeerDTO.getQuantity(), is(equalTo(expectedBeerDTO.getQuantity())));
 
         assertThat(createdBeerDTO.getQuantity(), is(greaterThan(2)));
+    }
+
+    @Test
+    void whenAlreadyRegisteredBeerInformedThenAnExceptionShouldBeThrown() {
+        // given
+        BeerDTO expectedBeerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+        Beer duplicatedBeer  = beerMapper.toModel(expectedBeerDTO);
+
+        // when
+        when(beerRepository.findByName(expectedBeerDTO.getName())).thenReturn(Optional.of(duplicatedBeer));
+
+        // then
+        assertThrows(BeerAlreadyRegisteredException.class, () -> beerService.createBeer(expectedBeerDTO));
     }
 }
